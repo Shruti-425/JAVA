2c40894a0676b568937e655b35173e73cbeb85c1
Shruti-425 <ss.upes123@gmail.com>
2025-01-30 14:09:28 +0530

    Max and Min

---
+++ FindMaxMin.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class FindMaxMin {
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in); 
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt(); 
+        int[] nums = new int[n];
+        System.out.println("Enter " + n + " numbers:");
+        for (int i = 0; i < n; i++) 
+        {
+            nums[i] = scanner.nextInt();
+        }
+
+        int max = nums[0];
+        int min = nums[0];
+
+        for (int i = 1; i < n; i++) 
+        {
+            if (nums[i] > max) 
+            {
+                max = nums[i];
+            }
+            if (nums[i] < min) 
+            {
+                min =nums[i];
+            }
+        }
+        System.out.println("Max:"+ max+", Min:"+min);
+    }
+}
